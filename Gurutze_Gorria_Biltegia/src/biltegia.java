95374793b02ca486ad9c6241cb2778c351c7b58c
Ander <ikagil25rlhusurbil.eus>
2026-03-25 09:59:29 +0100

    klase batzuk egiten

---
+++ Gurutze_Gorria_Biltegia/src/biltegia.java
@@ -0,0 +1,20 @@
+public class biltegia {
+   private int id;
+   private String kodea;
+   private int pasiloZbk;
+   private String kokapenKodea;
+   
+   public elikagaiakGorde(Iragankorrak, erdiIragankorrak, ezIragankorrak){}//enum erabiliz
+
+   public produktuakGehitu(){}
+
+   public produktuBatGehitu(){}
+
+   public produtuakAldatu(){}//Identifikatzailearen arabera
+   
+   public produktuakEzabatu(){}//Identifikatzailearen arabera
+    
+   public produktuakBistaratu(){}
+
+}
+

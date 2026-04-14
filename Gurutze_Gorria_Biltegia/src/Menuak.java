2ffad4e5d77e2fae21e55d73acf037a393770d78
Ares <ikaperez25@lhusurbil.eus>
2026-04-14 13:51:27 +0200

    MENUAK JAVA

---
+++ Gurutze_Gorria_Biltegia/src/Menuak.java
@@ -0,0 +1,16 @@
+public class Menuak {
+
+    public static void erakutsiAdminMenua() {
+        System.out.println("\n--- ADMINISTRATZAILE MENUA ---");
+
+        // metodos con sus caracterisitucaS Y PERNIMISOW
+    }
+
+    public static void erakutsiSarreraIrteeraMenua() {
+        System.out.println("\n---  KUDEATZAILEA ---");
+    }
+
+    public static void erakutsiStockMenua() {
+        System.out.println("\n--- STOCK KUDEATZAILEA ---");
+    }
+}

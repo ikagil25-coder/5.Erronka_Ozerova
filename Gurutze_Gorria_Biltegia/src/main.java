c443aa4b6c93e0b9a054b7b3fb5e3ed11a20ddda
Ares <ikaperez25@lhusurbil.eus>
2026-04-22 13:48:46 +0200

    Aldaketa batzuk

--- Gurutze_Gorria_Biltegia/src/main.java
+++ Gurutze_Gorria_Biltegia/src/main.java
@@ -1,7 +1,24 @@
-
-
 public class Main {
     public static void main(String[] args) {
+        
+        Stock stockKudeaketa = new Stock();
+        SarrerakEtaIrteerak mugimenduak = new SarrerakEtaIrteerak();
+        
+       
+        System.out.println("\n--- 1. Produktuak Sortzen eta BBDD-ra gehitzen ---");
+        Iragankorra p1 = new Iragankorra(901, "REF-IR-901", "Esne freskoa", "Kaiku", 1, 1, 1, "2026-12-31", true);
+        ErdiIragankorra p2 = new ErdiIragankorra(902, "REF-ER-902", "Olioa", "Urzu", 2, 2, 2, "2027-06-30", false, 40.5);
+        EzIragankorra p3 = new EzIragankorra(903, "REF-EZ-903", "Lentejak", "Luengo", 3, 3, 3, false);
+        EzIragankorra p4 = new EzIragankorra(904, "REF-EZ-904", "Atuna", "Isabel", 4, 4, 3, true);
+        
+        stockKudeaketa.produktuaGehitu(p1);
+        stockKudeaketa.produktuaGehitu(p2);
+        stockKudeaketa.produktuaGehitu(p3);
+        stockKudeaketa.produktuaGehitu(p4);
+        
+       
+
+        System.out.println("--- Gurutze Gorria Biltegia - Sartu zure datuak ---");
         Login loginProzesua = new Login();
         String rol = loginProzesua.logina();
 
